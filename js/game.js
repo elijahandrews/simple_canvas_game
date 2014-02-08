22779d4fd13a3711f7c6acf930e09cdf8f8b257b
Elijah Andrews <elijahcandrews@gmail.com>
2014-02-08 15:48:22 -0800

    Check for player collision with wall

--- js/game.js
+++ js/game.js
@@ -64,16 +64,24 @@ var placeMonster = function () {
 // Update game objects
 var update = function (modifier) {
 	if (38 in keysDown) { // Player holding up
-		hero.y -= hero.speed * modifier;
+		if (hero.y >= 32) { // Check collision with top wall
+			hero.y -= hero.speed * modifier;
+		}
 	}
 	if (40 in keysDown) { // Player holding down
-		hero.y += hero.speed * modifier;
+		if (hero.y <= canvas.height - 64) { // Check collision with bottom wall
+			hero.y += hero.speed * modifier;
+		}
 	}
 	if (37 in keysDown) { // Player holding left
-		hero.x -= hero.speed * modifier;
+		if (hero.x >= 32) { // Check collision with left wall
+			hero.x -= hero.speed * modifier;
+		}
 	}
 	if (39 in keysDown) { // Player holding right
-		hero.x += hero.speed * modifier;
+		if (hero.x <= canvas.width - 64) {
+			hero.x += hero.speed * modifier;
+		}
 	}
 
 	// Are they touching?
